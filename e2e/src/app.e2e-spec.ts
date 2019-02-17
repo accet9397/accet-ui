7dc50f9153ecec172f571766627769f4ff8fe1e1
Premkumar Shanmugam <premkumar.shanmugam@gmail.com>
2019-02-17 08:32:16 +0530

    Update e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ACCET - Class of 1997');
+    expect(page.getTitleText()).toEqual('Welcome User');
   });
 
   afterEach(async () => {
